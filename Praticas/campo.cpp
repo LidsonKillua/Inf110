ebc1ba16444d29dae8539442fedb747085ae7d72
Lidson Oliveira <lidsonvini2@gmail.com>
2023-05-20 13:31:12 -0300

    correção para funcionar

--- Praticas/campo.cpp
+++ Praticas/campo.cpp
@@ -15,7 +15,7 @@ int main(){
 
   for(int i = 0; i < b; i++){
     cin >> xB >> yB;
-    v[xB][yB] = 10;
+    v[xB-1][yB-1] = 10;
   }
 
   /* Mostrar a matriz
@@ -27,25 +27,30 @@ int main(){
 
   for(int i = 0; i < m; i++){
     for(int j = 0; j < n; j++){
-      if(v[i][j] == 10)
-        cout << 'B';
-      else{
+      if(v[i][j] != 10){
         qtdB = 0;
 
         for(int k = ((i-1 < 0) ? i : i-1); (k <= i+1) && (k < m); k++)
-          for(int l = ((j-1 < 0) ? j : j-1); (l <= j+1) && (l < n); j++)
+          for(int l = ((j-1 < 0) ? j : j-1); (l <= j+1) && (l < n); l++)
             if(v[k][l] == 10)
               qtdB++;
 
-        if(qtdB > 0)
-          cout << qtdB;
-        else
-          cout << '-';
+        v[i][j] = qtdB;
       }
     }
+  }
+  
+  for(int i = 0; i < m; i++){
+    for(int j = 0; j < n; j++)
+      if(v[i][j] == 10)
+        cout << "B";
+      else if(v[i][j] == 0)
+        cout << "-";
+      else
+        cout << v[i][j];
+        
     cout << endl;
   }
 
   return 0;
 }
-
